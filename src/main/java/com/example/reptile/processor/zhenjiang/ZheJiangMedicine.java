8a29713cac6c85a83fc363fc8d4cbbdb7d94d6fd
shixianqing <1434501995@qq.com>
2018-10-14 22:20:19 +0800

    修改爬虫文件

--- src/main/java/com/example/reptile/processor/zhenjiang/ZheJiangMedicine.java
+++ src/main/java/com/example/reptile/processor/zhenjiang/ZheJiangMedicine.java
@@ -1,9 +1,11 @@
 package com.example.reptile.processor.zhenjiang;
 
+import com.example.reptile.pipeline.ZheJiangMedicinePipeline;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 import us.codecraft.webmagic.selector.Html;
 
@@ -14,13 +16,13 @@ import java.text.MessageFormat;
  */
 public class ZheJiangMedicine implements PageProcessor {
 
-    private String startUrl = "http://www.zjylbx.com/drug_catalogue.aspx";
+    private static String startUrl = "http://www.zjylbx.com/drug_catalogue.aspx";
 
     private String startUrlRegex = "http://www\\.zjylbx\\.com/drug_catalogue.aspx";
 
     private String pageCommonUrl = "http://www.zjylbx.com/drug_catalogue.aspx?page={0}";
 
-    private String pageCommonUrlRegex = "http://www\\.zjylbx\\.com/drug_catalogue.aspx?page=[0-9]+";
+    public static String pageCommonUrlRegex = "http://www\\.zjylbx\\.com/drug_catalogue.aspx\\?page=[0-9]+";
 
     private Site site = Site.me().setCycleRetryTimes(3).setTimeOut(120000).setSleepTime((int)(Math.random()*1000 + 4000)).setUserAgent( (int)(Math.random()*2 ) == 1 ? "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134");
 
@@ -42,5 +44,9 @@ public class ZheJiangMedicine implements PageProcessor {
     @Override
     public Site getSite() {
         return site;
+    }
+
+    public static void main(String[] args) {
+        Spider.create(new ZheJiangMedicine()).addUrl(startUrl).thread(5).addPipeline(new ZheJiangMedicinePipeline()).run();
     }
 }
